3666f1b41156189ce9f90f5c08c7450eaba1045c
Gary Riger <grigerny@aol.com>
2013-11-22 07:11:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elpuntonetwork::Application.config.secret_token = '818a4f6d2decfe68bce4513d837587f621e10481d62f88c645c0bf4b264bfcd9105935093ecf332f99d144f4d7eb4d5f86b4685820abfb15ecb5cdd2c966a37b'
