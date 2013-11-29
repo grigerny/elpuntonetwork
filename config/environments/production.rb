737a8265d2775cdffd593178ae7962b203404735
Gary Riger <grigerny@aol.com>
2013-11-29 17:03:26 -0500

    made changes to config.action_mailer.default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Elpuntonetwork::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.assets.precompile += %w( print.css )
   
-  config.action_mailer.default_url_options = { :host => 'http://vast-tor-4154.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'vast-tor-4154.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
