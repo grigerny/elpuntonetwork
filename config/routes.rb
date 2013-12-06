b6b5d79ae5bbc254a510387323ccf5ea132337a4
Gary Riger <grigerny@aol.com>
2013-12-06 17:11:48 -0500

    added home#index page

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ Elpuntonetwork::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'categories#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
